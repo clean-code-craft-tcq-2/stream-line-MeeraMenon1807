c457174bb2f9d00adbce8af20276fa7f31e50350
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-04-21 20:38:58 +0530

    Create receiver.h

---
+++ receiver/receiver.h
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+#define NUMBEROFSAMPLE 50
+
+struct batteryParameters
+{
+    int MaxValue=0;
+    int MinValue =0;
+    float MovingAVg=0;
+};
+
+int FindMAXValueWithCurrentSample(int OldMaxValue, int CurrentSample);
+int FindMinValueWithCurrentSample(int OldMaxValue, int CurrentSample);
+float findMovingAvgOfLastFiveSamples(int *dataSample, int sampleIndex);
+bool printStatisticOnConsole(batteryParameters SOC,batteryParameters Temperature);
