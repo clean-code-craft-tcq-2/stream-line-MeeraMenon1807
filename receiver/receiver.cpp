80a21b436000f1daeec32be4b96cff8af40a63bf
M-Raksha <91051247+M-Raksha@users.noreply.github.com>
2022-04-22 08:12:19 +0530

    Update receiver.cpp

--- receiver/receiver.cpp
+++ receiver/receiver.cpp
@@ -68,6 +68,10 @@ bool ReadStreamingDataAndComputeStatistics(int *SOCDatastream, int *TemperatureD
     getStreamingData(TemperatureDatastream);
     getStreamingData(SOCDatastream);
     
+    StateOFCharge.MaxValue = SOCDatastream[0];
+    StateOFCharge.MinValue = SOCDatastream[0];
+    Temperature.MaxValue = TemperatureDatastream[0];
+    Temperature.MinValue = TemperatureDatastream[0];
     
      for(sampleIndex=0;sampleIndex<NUMBEROFSAMPLE;sampleIndex++)
   {
@@ -86,36 +90,4 @@ bool ReadStreamingDataAndComputeStatistics(int *SOCDatastream, int *TemperatureD
 
 
 
-/*
-#define STREAM_LENGTH 50
 
-void getSingleStreamOfValues(int dataStream[])
-{
-    int idx;
-    scanf("%*s", &dataStream); // Skip parameter string at beginning of stream
-    for (idx = 0; idx < STREAM_LENGTH; idx++)
-    {
-        scanf("%i", &dataStream[idx]);
-    }
-}
-
-
-int main()
-{
-  int temperatureDataStream[STREAM_LENGTH];
-   int temperatureDataStream1[STREAM_LENGTH];
-  int i;
-  getSingleStreamOfValues(&temperatureDataStream[0]);
-   getSingleStreamOfValues(&temperatureDataStream1[0]);
-  for(i=0;i<STREAM_LENGTH;i++)
-  {
-    printf("%d ",temperatureDataStream[i] );
-  }
- for(i=0;i<STREAM_LENGTH;i++)
-  {
-    printf("%d ",temperatureDataStream1[i] );
-  }
- 
-  return 0;
-}
-*/
